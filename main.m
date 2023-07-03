b127e464f91b85ec6a193a96bca424a59dca6897
Tony Gorez <gorez.tony@gmail.com>
2023-07-03 10:51:04 +0200

    chore: format

--- main.m
+++ main.m
@@ -1,28 +1,31 @@
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
-@property (strong) NSStatusItem *statusItem;
+@property(strong) NSStatusItem *statusItem;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-  self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-  
+  self.statusItem = [[NSStatusBar systemStatusBar]
+      statusItemWithLength:NSVariableStatusItemLength];
+
   self.statusItem.button.title = @"Hello";
   self.statusItem.highlightMode = YES;
   self.statusItem.menu = [[NSMenu alloc] initWithTitle:@"Menu"];
 
-  [self.statusItem.menu addItemWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@""];
+  [self.statusItem.menu addItemWithTitle:@"Quit"
+                                  action:@selector(terminate:)
+                           keyEquivalent:@""];
 }
 
 - (void)terminate:(id)sender {
-    [NSApp terminate:sender];
+  [NSApp terminate:sender];
 }
 
 @end
 
-int main(int argc, const char* argv []) {
+int main(int argc, const char *argv[]) {
   AppDelegate *appDelegate = [[AppDelegate alloc] init];
   [NSApplication sharedApplication].delegate = appDelegate;
   return NSApplicationMain(argc, argv);
