686a4ee9d22857db916080196dbbd34d6e962f96
Tony Gorez <gorez.tony@gmail.com>
2023-07-10 14:17:39 +0200

    feat: play with menu
    * change icon
    * change label
    * disable item
    * toggle item

--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+#include <AppKit/AppKit.h>
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
@@ -29,29 +30,90 @@
                                                            action:nil
                                                     keyEquivalent:@""];
   NSMenu *settingMenu = [[NSMenu alloc] initWithTitle:@"Settings"];
-  [settingMenu addItemWithTitle:@"Launch at startup"
-                         action:nil
-                  keyEquivalent:@""];
+  [settingMenu addItemWithTitle:@"Nested item" action:nil keyEquivalent:@""];
   [settingMenuItem setSubmenu:settingMenu];
+
+  NSMenuItem *launchAtStartupMenuItem =
+      [[NSMenuItem alloc] initWithTitle:@"Launch at startup"
+                                 action:@selector(changeCheck:)
+                          keyEquivalent:@""];
+  [launchAtStartupMenuItem setState:NSControlStateValueOn];
+
+  NSMenuItem *disableMenuItem =
+      [[NSMenuItem alloc] initWithTitle:@"Disable item below"
+                                 action:@selector(changeAccessibility:)
+                          keyEquivalent:@""];
 
   // Create items
   [mainMenu addItemWithTitle:@"Go to GitHub" action:nil keyEquivalent:@""];
   [mainMenu addItemWithTitle:@"Change icon"
                       action:@selector(changeIcon:)
                keyEquivalent:@""];
+  [mainMenu addItemWithTitle:@"Change label"
+                      action:@selector(changeLabel:)
+               keyEquivalent:@""];
   [mainMenu addItem:[NSMenuItem separatorItem]];
   [mainMenu addItem:settingMenuItem];
+  [mainMenu addItem:[NSMenuItem separatorItem]];
+  [mainMenu addItem:launchAtStartupMenuItem];
+  [mainMenu addItem:[NSMenuItem separatorItem]];
+  [mainMenu addItem:disableMenuItem];
+  [mainMenu addItemWithTitle:@"Enabled"
+                      action:@selector(noop:)
+               keyEquivalent:@""];
   [mainMenu addItem:[NSMenuItem separatorItem]];
   [mainMenu addItemWithTitle:@"Quit"
                       action:@selector(terminate:)
                keyEquivalent:@"q"];
 
+  // Really important to disable items individually
+  mainMenu.autoenablesItems = NO;
+
   // Set menu
   self.statusItem.menu = mainMenu;
 }
 
+- (void)noop:(id)sender {
+}
+
+- (void)changeAccessibility:(id)sender {
+  NSMenuItem *item = self.statusItem.menu.itemArray[9];
+  if ([item isEnabled]) {
+    [item setTitle:@"Disabled"];
+    [item setEnabled:NO];
+  } else {
+    [item setTitle:@"Enabled"];
+    [item setEnabled:YES];
+  }
+}
+
+- (void)changeCheck:(id)sender {
+  NSMenuItem *item = self.statusItem.menu.itemArray[6];
+  if (item.state == NSControlStateValueOn) {
+    [item setState:NSControlStateValueOff];
+  } else {
+    [item setState:NSControlStateValueOn];
+  }
+}
+
+- (void)changeLabel:(id)sender {
+  NSString *title = self.statusItem.menu.itemArray[2].title;
+  if ([title isEqualToString:@"Change label"]) {
+    title = @"Changed";
+  } else {
+    title = @"Change label";
+  }
+  self.statusItem.menu.itemArray[2].title = title;
+}
+
 - (void)changeIcon:(id)sender {
-  NSImage *image = [NSImage imageNamed:@"icon"];
+  NSString *iconName = self.statusItem.button.image.name;
+  if ([iconName isEqualToString:@"icon"]) {
+    iconName = @"icon2";
+  } else {
+    iconName = @"icon";
+  }
+  NSImage *image = [NSImage imageNamed:iconName];
   image.size = NSMakeSize(18, 18);
   self.statusItem.button.image = image;
 }
